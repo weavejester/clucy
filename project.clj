9dc283a7135f0af7109439344770b4b60e1bb774
James Reeves <jreeves@weavejester.com>
2011-05-04 21:59:05 +0100

    Removed dev deps for lein-clojars and swank

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
 (defproject clucy "0.1.1-SNAPSHOT"
   :description "A Clojure interface to the Lucene search engine"
   :url "http://github/weavejester/clucy"
-  :dependencies [[org.apache.lucene/lucene-core "3.0.1"]]
-  :dev-dependencies [[lein-clojars "0.6.0"]
-                     [swank-clojure "1.3.0"]])
+  :dependencies [[org.apache.lucene/lucene-core "3.0.1"]])
