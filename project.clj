f75dc4ed30fdeffdfe593c6fbe6547020e3e452a
James Reeves <jreeves@weavejester.com>
2011-05-04 22:00:40 +0100

    Updated Lucene version to 3.0.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject clucy "0.1.1-SNAPSHOT"
   :description "A Clojure interface to the Lucene search engine"
   :url "http://github/weavejester/clucy"
-  :dependencies [[org.apache.lucene/lucene-core "3.0.1"]])
+  :dependencies [[org.apache.lucene/lucene-core "3.0.3"]])
