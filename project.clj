77698476eb5abc042bf3f336896b2077874e8d52
James Reeves <jreeves@weavejester.com>
2011-05-04 23:47:07 +0100

    Released 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clucy "0.1.1-SNAPSHOT"
+(defproject clucy "0.2.0"
   :description "A Clojure interface to the Lucene search engine"
   :url "http://github/weavejester/clucy"
   :dependencies [[org.clojure/clojure "1.2.1"]
