e56e3f837bb08239d1fbcf233fbddd32e66e523d
Phil Hagelberg <technomancy@gmail.com>
2011-05-31 21:40:56 -0700

    Release 0.2.1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clucy "0.2.1-SNAPSHOT"
+(defproject clucy "0.2.1"
   :description "A Clojure interface to the Lucene search engine"
   :url "http://github/weavejester/clucy"
   :dependencies [[org.clojure/clojure "1.2.1"]
