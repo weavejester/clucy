d1b0d98b3580ca84eb4e6e0e3a16b2ec7e977e05
Wolodja Wentland <w@babilen5.org>
2012-03-14 14:37:59 +0000

    Update to lein-multi 1.1.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.apache.lucene/lucene-core "3.0.3"]
                  [org.apache.lucene/lucene-highlighter "3.0.3"]]
-  :dev-dependencies [[lein-multi "1.0.0"]]
+  :dev-dependencies [[lein-multi "1.1.0"]]
   :multi-deps {"lucene2" [[org.clojure/clojure "1.2.1"]
                           [org.apache.lucene/lucene-core "2.9.2"]
                           [org.apache.lucene/lucene-highlighter "2.9.2"]]})
